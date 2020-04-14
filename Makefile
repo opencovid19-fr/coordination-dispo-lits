0ff79d6fe37623ad06fdeb8407078fced09bae18
Antoine <antoine.biard.10@gmail.com>
2020-04-14 19:08:47 +0200

    Add docker compose and Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	docker-compose -f docker-compose.yml -f docker-compose-test.yml run --rm server
--- docker/testdb/init-user-db.sh
+++ docker/testdb/init-user-db.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER docker with password 'docker';
-    CREATE DATABASE docker;
-    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
-EOSQL
+# Everything commented as we are not needing any startup configuration right now
+# #!/bin/bash
+# set -e
+#
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+#     CREATE USER docker with password 'docker';
+#     CREATE DATABASE docker;
+#     GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+# EOSQL
